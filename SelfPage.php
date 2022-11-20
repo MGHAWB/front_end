c53e3212ffa93ac00e4def96553f2a928bf16c8b
dwvwdv <dwvwdv@protonmail.com>
2022-11-20 10:25:43 +0800

    .

--- SelfPage.php
+++ SelfPage.php
@@ -1,117 +1,156 @@
 <!DOCTYPE html>
-<!-- saved from url=(0049)https://getbootstrap.com/docs/5.2/examples/cover/ -->
-<html lang="en" class="h-100"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.104.2">
-    <title>Cover Template · Bootstrap v5.2</title>
+<html lang="ar" dir="rtl"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/cover/">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+	<meta name="generator" content="Hugo 0.104.2">
+	<title>個人頁面</title>
 
-    
-
-    
-
-<link href="./SelfPage_files/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-
-    <!-- Favicons -->
-<link rel="apple-touch-icon" href="https://getbootstrap.com/docs/5.2/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-<link rel="icon" href="https://getbootstrap.com/docs/5.2/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-<link rel="icon" href="https://getbootstrap.com/docs/5.2/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
-<link rel="manifest" href="https://getbootstrap.com/docs/5.2/assets/img/favicons/manifest.json">
-<link rel="mask-icon" href="https://getbootstrap.com/docs/5.2/assets/img/favicons/safari-pinned-tab.svg" color="#712cf9">
-<link rel="icon" href="https://getbootstrap.com/docs/5.2/assets/img/favicons/favicon.ico">
-<meta name="theme-color" content="#712cf9">
+	<link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/blog-rtl/">
 
 
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
 
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
 
-      .b-example-divider {
-        height: 3rem;
-        background-color: rgba(0, 0, 0, .1);
-        border: solid rgba(0, 0, 0, .15);
-        border-width: 1px 0;
-        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
-      }
 
-      .b-example-vr {
-        flex-shrink: 0;
-        width: 1.5rem;
-        height: 100vh;
-      }
+<link href="./S_files/bootstrap.rtl.min.css" rel="stylesheet" integrity="sha384-7mQhpDl5nRA5nY9lr8F1st2NbIly/8WqhjTp+0oFxEA/QUuvlbF6M1KXezGBh3Nb" crossorigin="anonymous">
 
-      .bi {
-        vertical-align: -.125em;
-        fill: currentColor;
-      }
 
-      .nav-scroller {
-        position: relative;
-        z-index: 2;
-        height: 2.75rem;
-        overflow-y: hidden;
-      }
 
-      .nav-scroller .nav {
-        display: flex;
-        flex-wrap: nowrap;
-        padding-bottom: 1rem;
-        margin-top: -1px;
-        overflow-x: auto;
-        text-align: center;
-        white-space: nowrap;
-        -webkit-overflow-scrolling: touch;
-      }
-    </style>
+	<style>
+	  .bd-placeholder-img {
+		font-size: 1.125rem;
+		text-anchor: middle;
+		-webkit-user-select: none;
+		-moz-user-select: none;
+		user-select: none;
+	  }
 
-    
-    <!-- Custom styles for this template -->
-    <link href="./SelfPage_files/cover.css" rel="stylesheet">
+	  @media (min-width: 768px) {
+		.bd-placeholder-img-lg {
+		  font-size: 3.5rem;
+		}
+	  }
+
+	  .b-example-divider {
+		height: 3rem;
+		background-color: rgba(0, 0, 0, .1);
+		border: solid rgba(0, 0, 0, .15);
+		border-width: 1px 0;
+		box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+	  }
+
+	  .b-example-vr {
+		flex-shrink: 0;
+		width: 1.5rem;
+		height: 100vh;
+	  }
+
+	  .bi {
+		vertical-align: -.125em;
+		fill: currentColor;
+	  }
+
+	  .nav-scroller {
+		position: relative;
+		z-index: 2;
+		height: 2.75rem;
+		overflow-y: hidden;
+	  }
+
+	  .nav-scroller .nav {
+		display: flex;
+		flex-wrap: nowrap;
+		padding-bottom: 1rem;
+		margin-top: -1px;
+		overflow-x: auto;
+		text-align: center;
+		white-space: nowrap;
+		-webkit-overflow-scrolling: touch;
+	  }
+	</style>
+
+
+	<!-- Custom styles for this template -->
+	<link href="./S_files/css" rel="stylesheet">
+	<!-- Custom styles for this template -->
+	<link href="./S_files/blog.rtl.css" rel="stylesheet">
   </head>
-  <body class="d-flex h-100 text-center text-bg-dark vsc-initialized">
-    
-<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-  <header class="mb-auto">
-    <div>
-      <h3 class="float-md-start mb-0">Cover</h3>
-      <nav class="nav nav-masthead justify-content-center float-md-end">
-        <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="https://getbootstrap.com/docs/5.2/examples/cover/#">Home</a>
-        <a class="nav-link fw-bold py-1 px-0" href="https://getbootstrap.com/docs/5.2/examples/cover/#">Features</a>
-        <a class="nav-link fw-bold py-1 px-0" href="https://getbootstrap.com/docs/5.2/examples/cover/#">Contact</a>
-      </nav>
-    </div>
+  <body class="vsc-initialized">
+
+<div class="container">
+  <header class="blog-header lh-1 py-3">
+	<div class="row flex-nowrap justify-content-between align-items-center">
+	  <div class="col-4 pt-1">
+		<a class="link-secondary" href="./index.php">Logout</a>
+	  </div>
+	  <div class="col-4 text-center">
+		<p class="blog-header-logo text-dark">個人頁面</p>
+	  </div>
+	  <div class="col-4 d-flex justify-content-end align-items-center">
+	  </div>
+	</div>
   </header>
 
-  <main class="px-3">
-    <h1>Cover your page.</h1>
-    <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
-    <p class="lead">
-      <a href="https://getbootstrap.com/docs/5.2/examples/cover/#" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
-    </p>
-  </main>
+  <div class="nav-scroller py-1 mb-2">
 
-  <footer class="mt-auto text-white-50">
-    <p>Cover template for <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
-  </footer>
+  </div>
 </div>
 
+<main class="container">
+  <div class="p-4 p-md-5 mb-4 rounded text-bg-dark">
+	<div class="col-md-6 px-0">
+	  <h1 class="display-4 fst-italic">食物</h1>
+		<div class="row align-items-end">
+			<div class ="col-2"><button type="submit" class="btn btn-primary">查詢</button></div>
+			<div class="col-6"><input type="text" class="form-control" name="food"></div>
+		</div>
+<hr>
+	  <p class="lead mb-0"><a href="./Chart.php" class="text-white fw-bold">本周營養攝取量</a></p>
+	</div>
+  </div>
 
-    
-  
+  <div class="row mb-2">
+	<div class="col-md-6">
+	  <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+		<div class="col p-4 d-flex flex-column position-static">
+		  <strong class="d-inline-block mb-2 text-primary">分類</strong>
+		  <h3 class="mb-0">危害食物</h3>
+		  <div class="mb-1 text-muted">敘述</div>
+		</div>
+		<div class="col-auto d-none d-lg-block">
+		  <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: صورة مصغرة" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#f00"></rect><text x="50%" y="50%" fill="#000000" dy=".3em">危害食物</text></svg>
+
+		</div>
+	  </div>
+	</div>
+	<div class="col-md-6">
+	  <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+		<div class="col p-4 d-flex flex-column position-static">
+		  <strong class="d-inline-block mb-2 text-success">666</strong>
+		  <h3 class="mb-0">888</h3>
+		  <div class="mb-1 text-muted"> 11</div>
+		  <p class="mb-auto">777</p>
+
+		</div>
+		<div class="col-auto d-none d-lg-block">
+		  <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: صورة مصغرة" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#bf4"></rect><text x="50%" y="50%" fill="#000000" dy=".3em">改善食物</text></svg>
+
+		</div>
+	  </div>
+	</div>
+  </div>
+
+
+
+</main>
+
+
+
+
+
+
+
+
 
 </body></html>
